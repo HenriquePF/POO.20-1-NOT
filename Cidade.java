0becf47a1dd08f12255920802db932ef88f4562c
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2020-03-16 22:05:04 -0300

    Lista 1 - Exerc 3

--- Cidade.java
+++ Cidade.java
@@ -1,4 +1,60 @@
+public class Cidade{
+	// atributos
+	private String nome;
+	private String uf;
+	private int populacaoMasculina,
+		    	populacaoFeminina;
+	private boolean calculado;
 
-public class Cidade {
+	// construtores
+	public Cidade(){
+	}
+
+	public Cidade(String nome, int popMasc, int popFem){
+		this.setNome(nome);
+		this.setPopulacaoMasculina(popMasc);
+		this.setPopulacaoFeminina(popFem);
+	}
+
+	// métodos
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public void setUf(String uf){
+		this.uf = uf;
+	}
+
+	public void setPopulacaoMasculina(int valor){
+		this.populacaoMasculina = valor;
+	}
+
+	public void setPopulacaoFeminina(int valor){
+		this.populacaoFeminina = valor;
+	}
+
+	public void setCalculado(boolean valor){
+		this.calculado = valor;
+	}	
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public String getUf(){
+		return this.uf;
+	}
+
+	public int getPopulacaoMasculina(){
+		return this.populacaoMasculina;
+	}
+
+	public int getPopulacaoFeminina(){
+		return this.populacaoFeminina;
+	}
+
+	public boolean isCalculado(){
+		return this.calculado;
+	}
 
 }
