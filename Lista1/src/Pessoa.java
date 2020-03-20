d7638f8982057d7374e0e28f38d372a90f92928f
marcel <marcel@DESKTOP-6LB1203>
2020-03-20 19:43:41 -0300

    Organizando o repositório

--- Lista1/src/Pessoa.java
+++ Lista1/src/Pessoa.java
@@ -1,15 +1,16 @@
+import java.time.LocalDate;
+
 class Pessoa{
 	private String nome;
 	private char sexo;
 	private LocalDate dataNascimento;
-	private String estadoCivil = "Solteiro"; // 1a forma de fazer a inicializaÃ§Ã£o
+	private String estadoCivil = "Solteiro"; // 1a forma de fazer a inicialização
 
-	public Pessoa(String nome, char sexo, 
-			LocalDate data){
+	public Pessoa(String nome, char sexo, LocalDate data){
 		this.nome = nome;
 		this.setSexo(sexo);
 		this.dataNascimento = data;
-		this.estadoCivil = "Solteiro";  // 2a forma de fazer a inicializaÃ§Ã£o
+		this.estadoCivil = "Solteiro";  // 2a forma de fazer a inicialização
 	}
 	
 	public void setSexo(char valor){
