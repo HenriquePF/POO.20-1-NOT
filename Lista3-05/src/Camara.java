e106581fc0b5d713ae3cbc0ddfe4febcb3b16073
marcel <marcel@DESKTOP-6LB1203>
2020-04-27 22:20:38 -0300

    Lista 3 - Exerc 5 (para finalizar com a implementação da GUI)

---
+++ Lista3-05/src/Camara.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Camara {
+	private HashMap<Integer, Partido> partidos = new HashMap<>();
+
+	public void addPartido(Partido p) {
+		if (p == null) {
+			throw new IllegalArgumentException("Partido não pode ser nulo.");
+		}
+		if (this.partidos.containsKey(p.getNumero())) {
+			throw new IllegalArgumentException("Já há partido com este número.");
+		}
+		this.partidos.put(p.getNumero(), p);
+	}
+
+	public int getTotalProjApres() {
+		int total = 0;
+		for (Partido p : this.partidos.values()) {
+			total += p.getQtdProjApres();
+		}
+		return total;
+	}
+
+	public int getTotalProjAprov() {
+		int total = 0;
+		for (Partido p : this.partidos.values()) {
+			total += p.getQtdProjApres();
+		}
+		return total;
+	}
+
+	public Vereador getVereadorMenorDesempenho() {
+		// primeira estratégia para descobrir um determinado vereador
+		Vereador piorDeTodos = null;
+		for (Partido p : this.partidos.values()) {
+			Vereador ruim = p.getVereadorMenorDesempenho();
+			if (piorDeTodos == null || (ruim != null && ruim.getDesempenho() < piorDeTodos.getDesempenho())) {
+				piorDeTodos = ruim;
+			}
+		}
+		return piorDeTodos;
+	}
+
+	public Vereador getVereadorMaisProjAprov() {
+		// outra estratégia para descobrir um determinado vereador
+		Vereador maior = null;
+		for (Partido p : this.partidos.values()) {
+			ArrayList<Vereador> temporaria = p.getVereadores();
+			for (Vereador v : temporaria) {
+				if (maior == null || v.getQtdProjAprov() > maior.getQtdProjAprov()) {
+					maior = v;
+				}
+			}
+		}
+		return maior;
+	}
+
+	public double getMediaDesempenho() {
+		double total = 0;
+		int contaVereadores = 0;
+		for (Partido p : this.partidos.values()) {
+			total += (p.getMediaDesempenho() * p.getQtdVereadores());
+			contaVereadores += p.getQtdVereadores();
+		}
+		return total / contaVereadores;
+	}
+
+	public ArrayList<Vereador> getVereadoresAcimaMedia() {
+		ArrayList<Vereador> retorno = new ArrayList<>();
+		double media = this.getMediaDesempenho();
+		for (Partido p : this.partidos.values()) {
+			ArrayList<Vereador> temporaria = p.getVereadores();
+			for (Vereador v : temporaria) {
+				if (v.getDesempenho() > media) {
+					retorno.add(v);
+				}
+			}
+		}
+		return retorno;
+	}
+}
---
+++ Lista3-05/src/Vereador.java
@@ -0,0 +1,83 @@
+
+public class Vereador {
+	private String nome;
+	private Partido partido;
+	private int qtdProjApres, qtdProjAprov;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if (nome == null || nome.isEmpty()) {
+			throw new IllegalArgumentException("Nome do vereador inválido");
+		}
+		this.nome = nome;
+	}
+
+	public Partido getPartido() {
+		return partido;
+	}
+
+	public void setPartido(Partido partido) {
+		this.partido = partido;
+	}
+
+	public int getQtdProjApres() {
+		return qtdProjApres;
+	}
+
+	public void setQtdProjApres(int qtdProjApres) {
+		if (qtdProjApres < 0) {
+			throw new IllegalArgumentException("Quantidade de projetos apresentados não pode ser menor que zero");
+		}
+		this.qtdProjApres = qtdProjApres;
+	}
+
+	public int getQtdProjAprov() {
+		return qtdProjAprov;
+	}
+
+	public void setQtdProjAprov(int qtdProjAprov) {
+		if (qtdProjAprov < 0) {
+			throw new IllegalArgumentException("Quantidade de projetos aprovados não pode ser menor que zero");
+		}
+		this.qtdProjAprov = qtdProjAprov;
+	}
+
+	public double getIndiceTrabalho() {
+		if (this.qtdProjApres == 0) {
+			return 0;
+		}
+		if (this.qtdProjApres < 6) {
+			return 0.8;
+		}
+		if (this.qtdProjApres < 11) {
+			return 1.0;
+		}
+		if (this.qtdProjApres < 18) {
+			return 1.08;
+		}
+		return 1.22;
+	}
+
+	public double getIndiceTrabalho1() {
+		double indice = 0;
+		if (this.qtdProjApres == 0) {
+			indice = 0;
+		} else if (this.qtdProjApres < 6) {
+			indice = 0.8;
+		} else if (this.qtdProjApres < 11) {
+			indice = 1.0;
+		} else if (this.qtdProjApres < 18) {
+			indice = 1.08;
+		} else {
+			indice = 1.22;
+		}
+		return indice;
+	}
+
+	public double getDesempenho() {
+		return (this.qtdProjAprov / (double)this.qtdProjApres) * this.getIndiceTrabalho();
+	}				// int / double --> double
+}
